6b571c155c85dc5f81ccd9eb251497c6f5b8c1ea
dzikrisee <marlianawatiazzahra@gmail.com>
2025-07-03 10:38:25 +0700

    feat: detail menu

--- src/pages/menu/[id].tsx
+++ src/pages/menu/[id].tsx
@@ -2,6 +2,7 @@ import { IMenu } from '@/types/menu';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/db';
+import { Button } from '@/components/ui/button';
 
 const DetailMenu = () => {
   const router = useRouter();
@@ -10,9 +11,10 @@ const DetailMenu = () => {
   useEffect(() => {
     if (router.query.id) {
       const fetchMenu = async () => {
-        const { data, error } = await supabase.from('menus').select('*').eq('id', router.query.id);
+        const { data, error } = await supabase.from('menus').select('*').eq('id', router.query.id).single();
 
-        console.log(data);
+        if (error) console.log('error: ', error);
+        else setMenu(data);
       };
 
       fetchMenu();
@@ -20,7 +22,23 @@ const DetailMenu = () => {
   }, [router.query.id]);
   return (
     <div className="container mx-auto py-8">
-      <div className="flex gap-16"></div>
+      <div className="flex gap-16">
+        {menu && (
+          <div className="flex gap-16 items-center w-full">
+            <div className="w-1/2">
+              <img src={menu.image} alt={menu.name} width={1080} height={1080} className="w-full h-[70vh] object-cover rounded-2xl" />
+            </div>
+            <div className="w-1/2">
+              <h1 className="text-5xl font-bold mb-4">{menu.name}</h1>
+              <p className="text-xl mb-4">{menu.description}</p>
+              <div className="flex items-center gap-4">
+                <p className="text-4xl font-bold">${menu.price}</p>
+                <Button className="text-lg py-6 font-bold" size="lg">Buy Now</Button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
